73ee503da16dbdf9b4142027184699990adc8e21
subrotoice <subroto.iu@gmail.com>
2024-08-15 12:20:15 +0600

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        // Custom colors
+        hBlue: "#2e75b6", // A shade of blue
+        allBlue: "#4a90e2", // A shade of pinkish-red
+      },
+    },
+  },
+  plugins: [],
+};
